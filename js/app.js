5074cadde4a3162f9c1c9e019ccb21b1b743f9f9
Marcos Almeida <80133181+NessKawl@users.noreply.github.com>
2024-06-08 22:47:51 -0300

    v1.0.8
    - app.js criado para fazer a verificação dos dados de reserva;
    - Adicionada imagens sobrepostas na página home.

---
+++ js/app.js
@@ -0,0 +1,50 @@
+let nomeInput = document.querySelector('#nome')
+let telInput = document.querySelector('#tel')
+let diaInput = document.querySelector('#dia')
+let qtdPessoasInput = document.querySelector('#qtdPessoas')
+// let horarioInput = document.querySelector('#horario')
+let emailInput = document.querySelector('#email')
+let obsInput = document.querySelector('#obs')
+let btn = document.querySelector('#btn')
+let div = document.querySelector('#res')
+
+function verificaReserva () {
+    let nome = nomeInput.value
+    let tel = telInput.value
+    let dia = diaInput.value
+    let qtdPessoas = qtdPessoasInput.value
+    
+    clearClass();
+    
+    if (nome === '' || tel === '' || dia === '' || qtdPessoas === '') {
+        div.classList.add('alert')
+        div.classList.add('alert-danger')
+        div.innerHTML = 'ERRO: Dados inválidos'
+    } else {
+        div.classList.add('alert')
+        div.classList.add('alert-success')
+        div.innerHTML = 'Reserva efetuada com sucesso!'
+    }
+
+    clear()
+}
+
+function clearClass () {
+    div.classList.remove('alert')
+    div.classList.remove('alert-success')
+    div.classList.remove('alert-danger')
+}
+
+function clear () {
+    nomeInput.value = ' '
+    telInput.value = ' '
+    diaInput.value = ' '
+    qtdPessoasInput.value = ' '
+    // horarioInput.value = ' '
+    emailInput.value = ' '
+    obsInput.value = ' '
+
+    nomeInput.focus()
+}
+
+btn.addEventListener('click', verificaReserva)
